42b618bfd77dbe979fd1578ddde3a3b4fbd5d5c6
akansha_00011 <akanshathakur0799@gmail.com>
2024-07-29 10:51:03 +0530

    # 1 basics , operators and datatypes

--- kahani.py
+++ kahani.py
@@ -1,10 +1,10 @@
 kahani = " "
 while True:
    data = input("enter a story=>") 
-   if len(data) ==0:
-      print("the end")
+   if len(data) == 0:
+      print("the end!")
       break
    kahani += data + "\n"
 
-print("the real story:")
-print(kahani)
+with open('story.txt','w') as file:
+   file.write(kahani)
